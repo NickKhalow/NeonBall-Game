bc2054f9e29e22bf7f69903c1cd8dca476838763
NickKhalow <nickhalow@gmail.com>
2023-08-02 19:49:00 +0300

    gyro input

---
+++ Assets/Scripts/Inputs/GyroInput.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Inputs
+{
+    public class GyroInput : AbstractInput
+    {
+        public override Vector2 Direction()
+        {
+            var raw = Input.acceleration;
+            return new Vector2(raw.x, raw.z);
+        }
+    }
+}
